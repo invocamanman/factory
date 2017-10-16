fb692ada3933820eed2924814603a8446bca64e7
JaMateuC <jaumemateu96@gmail.com>
2017-10-16 12:18:37 +0200

    log4j

--- src/main/java/Factory.java
+++ src/main/java/Factory.java
@@ -1,2 +1,23 @@
 public class Factory {
+
+    private static Factory factory;
+
+    private static Factory getInstance(){
+
+        if(factory == null){
+
+            factory = new Factory();
+
+        }
+
+        return factory;
+
+    }
+
+    private Class getCommand(String class){
+
+
+
+    }
+
 }
